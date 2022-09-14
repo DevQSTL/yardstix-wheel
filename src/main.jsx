c2371611d5877553a834f0e432339e71825fb141
DevQSTL <devqstl@gmail.com>
2022-09-14 13:31:07 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import '@fontsource/montserrat'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/700.css'
+
+import '@fontsource/inter'
+import '@fontsource/inter/400.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
